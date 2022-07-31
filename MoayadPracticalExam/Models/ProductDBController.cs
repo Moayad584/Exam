a5b113999f19b81f45c35c2edfa8402d376d53d4
Moayad <moayad.hakim@gmail.com>
2022-07-31 11:07:49 +0300

    Created the DbContext

---
+++ MoayadPracticalExam/Models/ProductDBController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoayadPracticalExam.Models.Entities;
+
+namespace MoayadPracticalExam.Models
+{
+    public class ProductDBController : DbContext
+
+    {
+        public ProductDBController( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ProductsCategories> ProductsCategories { get; set; }
+
+        public DbSet<ApplicationUsers> ApplicationUsers { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<CartItem> CartItems { get; set; }
+
+        public DbSet<Products> Products { get; set; }
+
+
+    }
+}
